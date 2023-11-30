8fdd9bbe1b7a52b5fce05e874f549e69e4d362c1
Greg Korte <gregmkorte@gmail.com>
2023-11-30 11:42:32 -0600

    Adding images to database

--- scripts/database.js
+++ scripts/database.js
@@ -6,12 +6,16 @@ const database = {
       willItDehydrate: false,
       remedy:
         "Celebrate the small victories, move on to more challenging tasks.",
+      image:
+        "https://tinyfaces.files.wordpress.com/2011/12/rockybalboa01-tinyfaces.jpg",
     },
     {
       name: "Variable Virtuosity",
       sweat_level: 5,
       willItDehydrate: false,
       remedy: "Practice declaring and using variables in different contexts.",
+      image:
+        "https://tinyfaces.files.wordpress.com/2011/12/rockybalboa01-tinyfaces.jpg",
     },
     {
       name: "Loop Luminescence",
@@ -19,6 +23,8 @@ const database = {
       willItDehydrate: false,
       remedy:
         "Master different loop types (for, while) and their applications.",
+      image:
+        "https://tinyfaces.files.wordpress.com/2011/12/rockybalboa01-tinyfaces.jpg",
     },
     {
       name: "Function Finesse",
@@ -26,12 +32,16 @@ const database = {
       willItDehydrate: true,
       remedy:
         "Understand functions, practice writing and calling functions effectively.",
+      image:
+        "https://tinyfaces.files.wordpress.com/2011/12/rockybalboa01-tinyfaces.jpg",
     },
     {
       name: "Array Agility",
       sweat_level: 6,
       willItDehydrate: false,
       remedy: "Practice manipulating arrays, learn common array methods.",
+      image:
+        "https://tinyfaces.files.wordpress.com/2011/12/rockybalboa01-tinyfaces.jpg",
     },
     {
       name: "Object Orientation Odyssey",
@@ -39,18 +49,24 @@ const database = {
       willItDehydrate: true,
       remedy:
         "Dive into object-oriented programming, understand classes and inheritance.",
+      image:
+        "https://tinyfaces.files.wordpress.com/2011/12/rockybalboa01-tinyfaces.jpg",
     },
     {
       name: "Exception Elegance",
       sweat_level: 5,
       willItDehydrate: false,
       remedy: "Handle exceptions gracefully, learn error handling techniques.",
+      image:
+        "https://tinyfaces.files.wordpress.com/2011/12/rockybalboa01-tinyfaces.jpg",
     },
     {
       name: "String Sorcery",
       sweat_level: 6,
       willItDehydrate: false,
       remedy: "Master string manipulation, learn common string methods.",
+      image:
+        "https://tinyfaces.files.wordpress.com/2011/12/rockybalboa01-tinyfaces.jpg",
     },
     {
       name: "File Handling Finesse",
@@ -58,6 +74,8 @@ const database = {
       willItDehydrate: true,
       remedy:
         "Practice reading and writing files, understand file handling concepts.",
+      image:
+        "https://tinyfaces.files.wordpress.com/2011/12/rockybalboa01-tinyfaces.jpg",
     },
     {
       name: "Algorithmic Aptitude",
@@ -65,6 +83,8 @@ const database = {
       willItDehydrate: true,
       remedy:
         "Practice solving algorithms, understand different problem-solving strategies.",
+      image:
+        "https://tinyfaces.files.wordpress.com/2011/12/rockybalboa01-tinyfaces.jpg",
     },
     {
       name: "Version Control Victory",
@@ -72,18 +92,24 @@ const database = {
       willItDehydrate: false,
       remedy:
         "Learn the basics of Git, commit regularly and use version control effectively.",
+      image:
+        "https://tinyfaces.files.wordpress.com/2011/12/rockybalboa01-tinyfaces.jpg",
     },
     {
       name: "Testing Tenacity",
       sweat_level: 6,
       willItDehydrate: false,
       remedy: "Embrace unit testing, practice writing and running test cases.",
+      image:
+        "https://tinyfaces.files.wordpress.com/2011/12/rockybalboa01-tinyfaces.jpg",
     },
     {
       name: "Debugging Dexterity",
       sweat_level: 5,
       willItDehydrate: false,
       remedy: "Improve debugging skills, use debugging tools effectively.",
+      image:
+        "https://tinyfaces.files.wordpress.com/2011/12/rockybalboa01-tinyfaces.jpg",
     },
     {
       name: "Framework Familiarity",
@@ -91,6 +117,8 @@ const database = {
       willItDehydrate: true,
       remedy:
         "Learn the basics of the chosen framework, follow official documentation.",
+      image:
+        "https://tinyfaces.files.wordpress.com/2011/12/rockybalboa01-tinyfaces.jpg",
     },
     {
       name: "Project Planning Prowess",
@@ -98,6 +126,8 @@ const database = {
       willItDehydrate: false,
       remedy:
         "Understand project management, plan and organize tasks effectively.",
+      image:
+        "https://tinyfaces.files.wordpress.com/2011/12/rockybalboa01-tinyfaces.jpg",
     },
   ],
   tears: [
@@ -106,6 +136,8 @@ const database = {
       weep_level: 6,
       willYouQuit: false,
       remedy: "Practice coding regularly, focus on understanding the syntax.",
+
+      image: "https://media.timeout.com/images/105822026/750/422/image.jpg",
     },
     {
       name: "Callback Confusion",
@@ -113,12 +145,14 @@ const database = {
       willYouQuit: true,
       remedy:
         "Master asynchronous programming, understand callbacks and promises.",
+      image: "https://media.timeout.com/images/105942110/170/170/image.jpg",
     },
     {
       name: "CSS Grid Grief",
       weep_level: 7,
       willYouQuit: true,
       remedy: "Study CSS Grid thoroughly, practice layout design with grid.",
+      image: "https://media.timeout.com/images/120923/750/562/image.jpg",
     },
     {
       name: "Regex Regret",
@@ -126,6 +160,7 @@ const database = {
       willYouQuit: true,
       remedy:
         "Learn regular expressions step by step, use online regex tools for practice.",
+      image: "https://media.timeout.com/images/132837/750/562/image.jpg",
     },
     {
       name: "Debugging Despair",
@@ -133,12 +168,14 @@ const database = {
       willYouQuit: false,
       remedy:
         "Improve debugging skills, use console.log and debugging tools effectively.",
+      image: "https://media.timeout.com/images/120851/750/562/image.jpg",
     },
     {
       name: "API Anguish",
       weep_level: 8,
       willYouQuit: true,
       remedy: "Understand API documentation, practice making API calls.",
+      image: "https://media.timeout.com/images/106067398/750/422/image.jpg",
     },
     {
       name: "Responsive Design Regret",
@@ -146,12 +183,14 @@ const database = {
       willYouQuit: true,
       remedy:
         "Master responsive design principles, use media queries effectively.",
+      image: "https://media.timeout.com/images/100442787/750/562/image.jpg",
     },
     {
       name: "Closure Confusion",
       weep_level: 8,
       willYouQuit: true,
       remedy: "Study closures in JavaScript, understand lexical scoping.",
+      image: "https://media.timeout.com/images/100546853/750/562/image.jpg",
     },
     {
       name: "Database Dismay",
@@ -159,6 +198,7 @@ const database = {
       willYouQuit: false,
       remedy:
         "Learn database basics, practice SQL queries with real-world examples.",
+      image: "https://media.timeout.com/images/122227/750/562/image.jpg",
     },
     {
       name: "Version Control Vexation",
@@ -166,6 +206,7 @@ const database = {
       willYouQuit: true,
       remedy:
         "Master Git and version control, understand branching and merging.",
+      image: "https://media.timeout.com/images/121851/750/562/image.jpg",
     },
     {
       name: "Algorithm Anxiety",
@@ -173,6 +214,7 @@ const database = {
       willYouQuit: true,
       remedy:
         "Practice algorithms regularly, understand time and space complexity.",
+      image: "https://media.timeout.com/images/121669/750/562/image.jpg",
     },
     {
       name: "Memory Management Melancholy",
@@ -180,6 +222,7 @@ const database = {
       willYouQuit: true,
       remedy:
         "Learn memory management concepts, understand pointers and references.",
+      image: "https://media.timeout.com/images/122095/750/562/image.jpg",
     },
     {
       name: "Framework Frustration",
@@ -187,6 +230,7 @@ const database = {
       willYouQuit: true,
       remedy:
         "Master the basics of the chosen framework, follow official documentation.",
+      image: "https://media.timeout.com/images/105237311/750/562/image.jpg",
     },
     {
       name: "Build Tool Blues",
@@ -194,6 +238,7 @@ const database = {
       willYouQuit: false,
       remedy:
         "Understand build tools, automate repetitive tasks in the development process.",
+      image: "https://media.timeout.com/images/106063256/image.jpg",
     },
     {
       name: "Code Review Conundrum",
@@ -201,6 +246,7 @@ const database = {
       willYouQuit: true,
       remedy:
         "Embrace constructive criticism, learn from code reviews for improvement.",
+      image: "https://media.timeout.com/images/121929/750/562/image.jpg",
     },
   ],
   blood: [
@@ -210,6 +256,7 @@ const database = {
       willItDrainYou: true,
       remedy:
         "Think recursively, break down the problem into smaller sub-problems.",
+      image: "https://media.timeout.com/images/105456202/750/422/image.jpg",
     },
     {
       name: "Metaprogramming Maze",
@@ -217,12 +264,14 @@ const database = {
       willItDrainYou: true,
       remedy:
         "Understand code that writes code, study metaprogramming techniques.",
+      image: "https://media.timeout.com/images/122115/750/562/image.jpg",
     },
     {
       name: "Monads Mayhem",
       hemorrhage_level: 9,
       willItDrainYou: true,
       remedy: "Master the concept of monads in functional programming.",
+      image: "https://media.timeout.com/images/122039/750/562/image.jpg",
     },
     {
       name: "Category Theory Abyss",
@@ -230,6 +279,7 @@ const database = {
       willItDrainYou: true,
       remedy:
         "Dive into the abstract world of category theory and its applications in programming.",
+      image: "https://media.timeout.com/images/122119/750/562/image.jpg",
     },
     {
       name: "Kernel Hacking Havoc",
@@ -237,12 +287,14 @@ const database = {
       willItDrainYou: true,
       remedy:
         "Explore the intricacies of low-level system programming and kernel hacking.",
+      image: "https://media.timeout.com/images/103625898/750/562/image.jpg",
     },
     {
       name: "Quantum Computing Quagmire",
       hemorrhage_level: 9,
       willItDrainYou: true,
       remedy: "Wrap your head around quantum mechanics and quantum algorithms.",
+      image: "https://media.timeout.com/images/121673/750/562/image.jpg",
     },
     {
       name: "Neural Network Nightmare",
@@ -250,6 +302,7 @@ const database = {
       willItDrainYou: true,
       remedy:
         "Tackle complex neural network architectures and deep learning concepts.",
+      image: "https://media.timeout.com/images/122011/750/562/image.jpg",
     },
     {
       name: "Blockchain Bloodbath",
@@ -257,12 +310,14 @@ const database = {
       willItDrainYou: false,
       remedy:
         "Dive into the decentralized world of blockchain and smart contracts.",
+      image: "https://media.timeout.com/images/106069998/750/422/image.jpg",
     },
     {
       name: "Type System Tsunami",
       hemorrhage_level: 8,
       willItDrainYou: true,
       remedy: "Master advanced type systems and type-level programming.",
+      image: "https://media.timeout.com/images/106026624/750/422/image.jpg",
     },
     {
       name: "Compiler Construction Chaos",
@@ -270,6 +325,7 @@ const database = {
       willItDrainYou: true,
       remedy:
         "Build your own compiler and understand the internals of language translation.",
+      image: "https://media.timeout.com/images/122131/750/562/image.jpg",
     },
     {
       name: "Algorithmic Apocalypse",
@@ -277,6 +333,7 @@ const database = {
       willItDrainYou: true,
       remedy:
         "Confront highly complex algorithms with intricate time and space complexities.",
+      image: "https://media.timeout.com/images/120991/750/562/image.jpg",
     },
     {
       name: "Chaos Engineering Conundrum",
@@ -284,6 +341,7 @@ const database = {
       willItDrainYou: true,
       remedy:
         "Learn chaos engineering principles for building robust and resilient systems.",
+      image: "https://media.timeout.com/images/105472155/750/562/image.jpg",
     },
     {
       name: "Galois Theory Gridlock",
@@ -291,6 +349,7 @@ const database = {
       willItDrainYou: true,
       remedy:
         "Delve into abstract algebra with a focus on Galois theory and its applications.",
+      image: "https://media.timeout.com/images/105816758/750/562/image.jpg",
     },
     {
       name: "Compiler Optimization Oblivion",
@@ -298,6 +357,7 @@ const database = {
       willItDrainYou: true,
       remedy:
         "Optimize code generation and execution through compiler optimization techniques.",
+      image: "https://media.timeout.com/images/121121/750/562/image.jpg",
     },
     {
       name: "Haskell Hysteria",
@@ -305,6 +365,7 @@ const database = {
       willItDrainYou: true,
       remedy:
         "Embrace the functional programming paradigm with the Haskell programming language.",
+      image: "https://media.timeout.com/images/103967993/750/562/image.jpg",
     },
   ],
 };
